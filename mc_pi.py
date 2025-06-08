8b9ea6458b95959b30d3d3672c9737804ad055bc
Rodrigo Ventura <rodrigo.ventura@isr.tecnico.ulisboa.pt>
2025-06-08 11:16:56 +0100

    Initial commit with a simple OpenMPI demo of computing π using Monte Carlo.

---
+++ mc_pi.py
@@ -0,0 +1,44 @@
+import time
+import numpy as np
+from mpi4py import MPI
+
+# Number of points per process
+N = 10**9
+
+# Setup OpenMPI
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+if rank==0:
+    # Master process
+    t0 = time.time()
+    base = int(t0)
+else:
+    # Slave process
+    base = None
+
+# Ensure different seeds per process
+seed = rank + comm.bcast(base, root=0)
+np.random.seed(seed)
+
+print(f"I'm instance {rank} in {size} and my seed is {seed}.")
+
+# Main computation
+chunk = 10**6
+n_inside = 0
+for nc in (N//chunk) * [chunk] + [N%chunk]:
+    # Dividing the computation in chunks
+    points = np.random.uniform(-1, 1, (nc,2))
+    inside = (np.sum(points**2, axis=1) <= 1)
+    n_inside += np.sum(inside)
+
+# Collect results by summing all n_inside
+total_inside = comm.reduce(n_inside, op=MPI.SUM, root=0)
+
+if rank==0:
+    # Master process
+    t = time.time() - t0
+    result = 4 * total_inside / (N*size)
+    error = np.abs(result - np.pi)
+    print(f"My approximation of π is {result} (error is {error}.\nIt took {t} seconds.")
